cb3c40241713b3dd4ce5812061af14653971bcca
JosephAMumford <JosephAMumford@gmail.com>
2018-03-10 21:48:02 -0600

    add new scripts

---
+++ MEAN/JSFundamentals/platform/math.js
@@ -0,0 +1,19 @@
+let x = Math.floor(Math.random() * 20);
+console.log(x);
+
+let y = Math.floor(Math.random() * 21);
+console.log(y);
+
+let z = Math.floor(Math.random() * 20) + 1;
+console.log(z);
+
+x = 11;
+y = 5;
+z = x % y;
+console.log(z);
+
+x=16;
+console.log(x % 2);
+
+y=17;
+console.log(y % 2);
---
+++ MEAN/JSFundamentals/platform/strings.js
@@ -0,0 +1,18 @@
+let x = "Hello World";
+let y = "Hello World";
+
+let firstName = "Oscar";
+let lastName = "Vasquez";
+let message = "Hello, my name is " + firstName + " " + lastName;
+console.log(message);
+
+let message2 = `Hello, my name is ${firstName} ${lastName}`;
+console.log(message2);
+
+let haiku = "Having been erased, \nThe document you're seeking \nMust now be retyped."
+console.log(haiku);
+
+let text = 'Hello I\'m Oscar';
+console.log(text);
+
+console.log('\u263A        \u2603        \u272f');

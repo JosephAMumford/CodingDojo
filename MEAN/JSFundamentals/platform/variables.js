d4256f3aa62cb8fbffe3d37b76375787b81673ba
JosephAMumford <JosephAMumford@gmail.com>
2018-03-10 19:25:12 -0600

    variables

---
+++ MEAN/JSFundamentals/platform/variables.js
@@ -0,0 +1,13 @@
+var myName = 'Kadie';
+var myAge = 29;
+var myBirthday;
+console.log('Hello World!');
+console.log(myName);
+console.log(myAge);
+console.log(myBirthday);
+
+var x = [1,2,3];
+var y = x;
+x.push('Hello world!')
+console.log(x);
+console.log(y);

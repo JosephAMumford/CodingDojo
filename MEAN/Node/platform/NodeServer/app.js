f6238c45d2afcbd49c7bdae9dab6cfdbf8f5fb79
JosephAMumford <JosephAMumford@gmail.com>
2018-03-18 17:48:35 -0500

    add assignments

---
+++ MEAN/Node/platform/NodeServer/app.js
@@ -0,0 +1,48 @@
+// get the http module:copy
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === "/dojo.html") {
+        fs.readFile('dojo.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/stylesheets/style.css") {
+        fs.readFile('stylesheets/style.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/css'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 6789");
+
+/*
+File type:	Headers:
+HTML	{'Content-Type': 'text/html'}
+CSS	{'Content-Type': 'text/css'}
+Javascript	{'Content-Type': 'text/javascript'}
+png/jpeg/gif	{'Content-Type': 'image/*'} (* is the image format, ie png or jpeg etc)
+*/
